a3c428ca6335272918cfe8e97c548a9a15b8f051
mynk717 <63432054+mynk717@users.noreply.github.com>
2025-08-01 10:13:35 +0000

    remove the signInAnonymously call

--- src/app/tata-aig-insurance/page.tsx
+++ src/app/tata-aig-insurance/page.tsx
@@ -1,11 +1,7 @@
-import { getAuth, signInAnonymously } from "firebase/auth";
 import { db } from "@/lib/firebase";
 import { collection, query, where, getDocs } from "firebase/firestore";
 
 export default async function Page() {
-  const auth = getAuth();
-  await signInAnonymously(auth); // Temporary authentication to test permissions
-
   const q = query(collection(db, "pillarPages"), where("slug", "==", "post-42"));
   const querySnapshot = await getDocs(q);
   const data = querySnapshot.docs.map((doc) => doc.data());
